ffd3038ec246e721b7a4bd4123b4d3018fb20ca9
Fernando Nobre <fernobre@gmail.com>
2011-04-19 14:38:08 -0300

    update

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -2,16 +2,36 @@ require "rubygems"
 require "twitter/model"
 require "twitter/constants"
 
+##
+#Twitter module
+##
 module Twitter
   class << self
-    
+  
+  ##
+  # Test method, does not do anything usefull
+  ##  
     def twitterTest
       return "this is the twitter gem speaking..."
     end
-    
-    def getAccount
-      return Twitter::Model::TwitterModel.all
+  
+  ##
+  # Returns the twitter config for a given area
+  ##
+    def getTwitter (area)
+      if(area.is_a? Numeric)
+        return Twitter::Model::TwitterModel.where(area_id: area)
+      else
+        return nil
+      end
     end
+    
+  ##
+  # Returns the twitter config when no area is defined
+  ##
+  def getTwitter
+    return Twitter::Model::TwitterModel.first
+  end
     
   end
 end
--- lib/twitter/model.rb
+++ lib/twitter/model.rb
@@ -7,6 +7,9 @@ module Twitter
       field :account_name
       field :priority, type :Integer
       field :area_id, type :Integer
+      
+      validates_numericality_of :area_id, only_integer :true
+      validates_numericality_of :priority, only_integer :true
     end
   
   end
