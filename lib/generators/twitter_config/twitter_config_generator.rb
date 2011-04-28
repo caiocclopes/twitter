395ba448ed1552246d722e37df86d2f34f6dd53f
Bruno Nigro <bruno.nigro@gmail.com>
2011-04-28 12:22:53 -0300

    New generator twitter_config

--- lib/generators/twitter_config/twitter_config_generator.rb
+++ lib/generators/twitter_config/twitter_config_generator.rb
@@ -6,9 +6,9 @@ class TwitterConfigGenerator < Rails::Generators::Base
   def generate_scaffold
     generate("scaffold", "twitter_config search_term:string account_name:string priority:integer area_id:integer")
     remove_file "/app/models/twitter_config.rb"
-    puts default_source_root
-    copy_file File.join(default_source_root, "twitter_config_model"), "/app/models/twitter_config.rb"
-    copy_file File.join(default_source_root, "twitter_config_iPhone"), "/app/controllers/twitter_controller.rb"
+    puts source_root
+    copy_file File.join(source_root, "twitter_config_model"), "/app/models/twitter_config.rb"
+    copy_file File.join(source_root, "twitter_config_iPhone"), "/app/controllers/twitter_controller.rb"
   end
 
 end
