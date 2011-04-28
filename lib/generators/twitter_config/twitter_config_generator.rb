39737debc27b8756db8426cf9e531718ac6dd848
Bruno Nigro <bruno.nigro@gmail.com>
2011-04-28 11:37:45 -0300

    New generator twitter_config

--- lib/generators/twitter_config/twitter_config_generator.rb
+++ lib/generators/twitter_config/twitter_config_generator.rb
@@ -6,10 +6,8 @@ class TwitterConfigGenerator < Rails::Generators::Base
   def generate_scaffold
     generate("scaffold", "twitter_config search_term:string account_name:string priority:integer area_id:integer")
     remove_file "/app/models/twitter_config.rb"
-    create_file "/app/models/twitter_config.rb", "class TwitterConfig < Twitter::Model::TwitterModel
-                ##Place any application specific fields in here
-                #example: field :name, type: String
-                end"
+    copy_file "twitter_config_model", "/app/models/twitter_config.rb"
+    copy_file "twitter_config_iPhone", "/app/controllers/twitter_controller.rb"
   end
 
 end
