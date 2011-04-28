3a220a89f0fecc712b4bdc3dbb18425416f77277
Bruno Nigro <bruno.nigro@gmail.com>
2011-04-28 10:20:10 -0300

    New generator twitter_config

--- lib/generators/twitter_config/twitter_config_generator.rb
+++ lib/generators/twitter_config/twitter_config_generator.rb
@@ -1,7 +1,7 @@
 # Use this generator like this:
 # rails g twitter_config
 
-class TwitterConfig < Rails::Generators::Base
+class TwitterConfigGenerator < Rails::Generators::Base
 
   def generate_scaffold
     generate("scaffold", "twitter_config search_term:string account_name:string priority:integer area_id:integer")
